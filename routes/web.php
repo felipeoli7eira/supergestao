d3bd504b95d57a710e67744ac58eccf7cba0cda9
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-02-01 19:21:50 -0300

    Home, Contato e Sobre: Rotas e Controladores

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Home;
+use App\Http\Controllers\About;
+use App\Http\Controllers\Contact;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [Home::class, 'index']);
+
+Route::get('/sobre', [About::class, 'index']);
+
+Route::get('/contato', [Contact::class, 'index']);
